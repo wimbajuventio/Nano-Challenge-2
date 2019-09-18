bc4936d33b4b9df618bd119022963026f709422b
wimba juventio <wimbajuventio@yahoo.com>
2019-09-18 14:06:17 +0700

    nano challange authorization

--- Nano Challenge 2/ViewController.swift
+++ Nano Challenge 2/ViewController.swift
@@ -7,14 +7,26 @@
 //
 
 import UIKit
-
+ import HealthKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
-
-
+        if HKHealthStore.isHealthDataAvailable() {
+            // Add code to use HealthKit here.
+            let healthStore = HKHealthStore()
+            let allTypes = Set([HKObjectType.workoutType(),
+                                HKObjectType.quantityType(forIdentifier: .activeEnergyBurned)!,
+                                HKObjectType.quantityType(forIdentifier: .distanceCycling)!,
+                                HKObjectType.quantityType(forIdentifier: .dietaryWater)!,
+                                HKObjectType.quantityType(forIdentifier: .distanceWalkingRunning)!,
+                                HKObjectType.quantityType(forIdentifier: .heartRate)!])
+            healthStore.requestAuthorization(toShare: allTypes, read: allTypes) { (success, error) in
+                if !success {
+                    // Handle the error here.
+                }
+            }
 }
-
+}
+}
